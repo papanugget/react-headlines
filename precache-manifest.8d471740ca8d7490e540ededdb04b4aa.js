a1e406116a81e3ddea3983d423b35d74955b0e60
papanugget <evilgenius04@gmail.com>
2019-04-25 09:38:50 -0400

    Updates

---
+++ precache-manifest.8d471740ca8d7490e540ededdb04b4aa.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28eab311763cd008bd46198628f94b58",
+    "url": "/react-headlines/index.html"
+  },
+  {
+    "revision": "5e6000cea1873ac72c3e",
+    "url": "/react-headlines/static/js/2.2ae674de.chunk.js"
+  },
+  {
+    "revision": "6b2a033dfdc749b04b3c",
+    "url": "/react-headlines/static/js/main.07d6e837.chunk.js"
+  },
+  {
+    "revision": "bdb50a1a4b5b0cbb4890",
+    "url": "/react-headlines/static/js/runtime~main.12e505dd.js"
+  }
+]);
