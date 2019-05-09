1489cf3b896e3f8a02abccc2de73ca4f9b39b882
papanugget <evilgenius04@gmail.com>
2019-05-09 10:00:51 -0400

    smooth scrolling test

--- src/components/FloatingButton.js
+++ src/components/FloatingButton.js
@@ -3,11 +3,15 @@ import '../css/floatingbutton.css';
 
 class FloatingButton extends React.Component {
     render() {
+        const smoothScroll =
+            document.querySelector('#sources').scrollIntoView({
+                behavior: 'smooth'
+        });
         document.querySelector('#sources').scrollIntoView({
             behavior: 'smooth'
         });
         return (
-            <a className="circular ui fixed bottom sticky pink icon button right floated" id="button-up" href="#sources">
+            <a className="circular ui fixed bottom sticky pink icon button right floated" id="button-up" onClick={this.smoothScroll}>
                 <i className="icon angle arrow double up"></i>
             </a>
         )
